04cb577ba776df14b00a41f16a1cdbef7a91c720
Peter Williams <pedroeldiablo@yahoo.com>
2019-12-16 00:37:08 +0000

    starting refactor to functional components with hooks

--- src/components/grid-node/grid-node.jsx
+++ src/components/grid-node/grid-node.jsx
@@ -1,34 +1,58 @@
-import React, {Component} from 'react';
+import React, { useState} from 'react';
 
 import './grid-node.css';
 
-export default class GridNode extends Component {
-  render() {
-    const {
-      col,
-      isFinish,
-      isStart,
-      isWall,
-      onMouseDown,
-      onMouseEnter,
-      onMouseUp,
-      row,
-    } = this.props;
-    const extraClassName = isFinish
-      ? 'node-finish'
-      : isStart
-      ? 'node-start'
-      : isWall
-      ? 'node-wall'
-      : '';
+export default function GridNode(props) {
+    const [isWall, updateIsWall] = useState(false)
+    const { isFinish, isStart, row , col} = props;
+    const extraClassName =  isFinish
+    ? 'node-finish'
+    : isStart
+    ? 'node-start'
+    : isWall
+    ? 'node-wall'
+    : '';
+
+    // debugger
+
+    function handleMouseEnter() {
+      // debugger
+      if (!props.mouseIsPressed) return;
+      updateIsWall(!isWall);
+      
+
+      // const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
+      // this.setState({grid: newGrid});
+    }
+
+  // render() {
+  //   const {
+  //     col,
+  //     isFinish,
+  //     isStart,
+  //     isWall,
+  //     row
+  //     // onMouseDown,
+  //     // onMouseEnter,
+  //     // onMouseUp,
+    
+  //   } = this.props;
+  //   
 
     return (
       <div
         id={`node-${row}-${col}`}
         className={`node ${extraClassName}`}
-        onMouseDown={() => onMouseDown(row, col)}
+        onMouseEnter={() => handleMouseEnter()}
+
+        
+        >
+
+        {/* onMouseDown={() => onMouseDown(row, col)}
         onMouseEnter={() => onMouseEnter(row, col)}
-        onMouseUp={() => onMouseUp()}></div>
+        onMouseUp={() => onMouseUp()}> */}
+          
+        </div>
     );
-  }
+  // }
 }
